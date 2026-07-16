f3b7f775293fdf345c3770c7a24a3460d3ef765b
YourName <youremail@example.com>
2026-07-16 18:37:08 +0300

    datadone

---
+++ backend/config/prisma.js
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"], // يطبع الاستعلامات والأخطاء في الـ Terminal للمساعدة في التطوير
+});
+
+export default prisma;
